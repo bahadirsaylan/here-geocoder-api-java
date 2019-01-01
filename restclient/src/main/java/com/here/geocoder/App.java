c5b632b5362f83ffe8ab6c670a4454c57882c739
Bahadir SAYLAN <bahadirsaylan@gmail.com>
2019-01-01 17:40:22 +0300

    Initial commit

---
+++ restclient/src/main/java/com/here/geocoder/App.java
@@ -0,0 +1,13 @@
+package com.here.geocoder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
