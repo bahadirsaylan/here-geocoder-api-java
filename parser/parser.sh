30d851c8cfb047d02ea250d639670bb700875061
bahadirsaylan <bahadirsaylan@gmail.com>
2019-01-02 12:34:00 +0300

    sh files updated

--- parser/parser.sh
+++ parser/parser.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java -cp target/parser-1.0-SNAPSHOT-jar-with-dependencies.jar com.here.geocoder.ParserApp ~/dist/scraping/develop-SNAPSHOT/out/stately/rent_detailed.csv com_here_geocoder
+INPUT=$1
+
+java -cp target/parser-1.0-SNAPSHOT-jar-with-dependencies.jar com.here.geocoder.ParserApp $INPUT com_here_geocoder
--- restclient/restclient.sh
+++ restclient/restclient.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-java -cp target/restclient-1.0-SNAPSHOT-jar-with-dependencies.jar com.here.geocoder.App ~/dist/scraping/develop-SNAPSHOT/out/stately/here_geocoded.csv 3
+OUTPUT=$1
+ADDRESS_COL=$2
+
+java -cp target/restclient-1.0-SNAPSHOT-jar-with-dependencies.jar com.here.geocoder.App $1 $2
